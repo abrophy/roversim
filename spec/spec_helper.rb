df131b8267e649348facb3859b81b05b83485d72
Arthur Brophy <artbrophy@gmail.com>
2018-08-19 23:34:15 +0200

    required rover class in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 require 'roversim_abrophy'
 
 require_relative '../lib/controller'
+require_relative '../lib/rover'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
