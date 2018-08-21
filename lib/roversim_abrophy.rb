167a52461708dcc0ae77b955352df3b4c8744674
Arthur Brophy <artbrophy@gmail.com>
2018-08-21 11:08:37 +0200

    preliminary topmost script correctly predicting rover destination

--- lib/roversim_abrophy.rb
+++ lib/roversim_abrophy.rb
@@ -1,5 +1,23 @@
 require 'roversim_abrophy/version'
 
-# simulates and predicts rover movement
-module RoversimAbrophy
+require_relative './controller'
+require_relative './rover'
+require_relative './zone'
+require_relative './tracers/north'
+require_relative './tracers/east'
+require_relative './tracers/south'
+require_relative './tracers/west'
+
+# Generates classes from input and returns destination
+input_array = []
+until ARGF.eof
+  input = ARGF.gets.split("\n")
+  input_array.push(input[0])
 end
+
+zone = Zone.new(input_array[0])
+rover = Rover.new(zone, input_array[1])
+controller = Controller.new(rover)
+
+controller.process_instructions(input_array[2])
+puts controller.destination
