d8287cdac49d82e5cb5e1154c4cf8955bf8aeb86
Arthur Brophy <artbrophy@gmail.com>
2018-08-21 10:12:11 +0200

    increase/decrease coordinate methods in place for rover

--- lib/rover.rb
+++ lib/rover.rb
@@ -2,17 +2,34 @@ require 'roversim_abrophy/version'
 
 # Rover class that stores rover position state
 class Rover
-  attr_accessor :position, :direction
+  attr_accessor :direction, :x, :y
 
   def initialize(zone, position)
     @zone = zone
-    @position = [position[0], position[1]]
+    @x = position[0]
+    @y = position[1]
     @direction_key = position[2]
     @tracer = tracer
   end
 
   def direction
     @tracer.to_s
+  end
+
+  def increase_x
+    @x += 1
+  end
+
+  def decrease_x
+    @x -= 1
+  end
+
+  def increase_y
+    @y += 1
+  end
+
+  def decrease_y
+    @y -= 1
   end
 
   def turn_left
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,11 +1,12 @@
 RSpec.describe Rover do
   let(:zone_size) { [8, 8] }
   let(:position) { [1, 2, 'N'] }
-  let(:zone) { Zone.new(zone_size) }
+  let(:zone) { double }
   let(:rover) { Rover.new(zone, position) }
 
   it 'initializes with correct values' do
-    expect(rover.position).to eq([1, 2])
+    expect(rover.x).to eq(1)
+    expect(rover.y).to eq(2)
     expect(rover.direction).to eq('N')
   end
 
@@ -20,6 +21,34 @@ RSpec.describe Rover do
     it 'changes direction of rover correctly' do
       rover.turn_right
       expect(rover.direction).to eq('E')
+    end
+  end
+
+  describe '#increase_x' do
+    it 'increase x coordinate correctly' do
+      rover.increase_x
+      expect(rover.x).to eq(2)
+    end
+  end
+
+  describe '#decrease_x' do
+    it 'decreases x coordinate correctly' do
+      rover.decrease_x
+      expect(rover.x).to eq(0)
+    end
+  end
+
+  describe '#increase_y' do
+    it 'increases y coordinate correctly' do
+      rover.increase_y
+      expect(rover.y).to eq(3)
+    end
+  end
+
+  describe '#decrease_y' do
+    it 'decreases y coordinate correctly' do
+      rover.decrease_y
+      expect(rover.y).to eq(1)
     end
   end
 end
